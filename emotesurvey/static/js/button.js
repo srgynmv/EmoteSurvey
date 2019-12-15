326eb852fb1a9d20ed42c42911db8090221db3a2
Liubov Neimushcheva <liubov23942@gmail.com>
2019-12-15 20:24:06 +0300

    Updated the styles and survey page

--- emotesurvey/static/js/button.js
+++ emotesurvey/static/js/button.js
@@ -1,15 +1,15 @@
-function createButton(context, func) {
-    var button = document.createElement("input");
-    button.type = "button";
-    button.value = "button";
-    button.onclick = func;
-    context.appendChild(button);
-}
+// function createButton(context, func) {
+//     var button = document.createElement("input");
+//     button.type = "button";
+//     button.value = "button";
+//     button.onclick = func;
+//     context.appendChild(button);
+// }
 
-window.onload = function() {
-    // createButton(document.body, function() {
-    //     //highlight(this.parentNode.childNodes[1]);
-    //     // Example of different context, copied function etc
-    //     // createButton(this.parentNode, this.onclick);
-    // });
-};
+// window.onload = function() {
+//     createButton(document.body, function() {
+//         //highlight(this.parentNode.childNodes[1]);
+//         // Example of different context, copied function etc
+//         // createButton(this.parentNode, this.onclick);
+//     });
+// };
--- emotesurvey/survey/views.py
+++ emotesurvey/survey/views.py
@@ -2,8 +2,10 @@ from django.http import HttpResponse
 from django.template.response import TemplateResponse
 from django.shortcuts import render
 
+
 def index(request):
     return TemplateResponse(request, "index.html")
+
 
 def survey(request):
     return render(request, "survey.html")
