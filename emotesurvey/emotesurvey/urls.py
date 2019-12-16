081ccea3ad48591ccd8772765b184bcabe8e3d15
Liubov Neimushcheva <liubov23942@gmail.com>
2019-12-16 20:52:16 +0300

    Removed obsolete resources Added redirect to the survey

--- emotesurvey/emotesurvey/urls.py
+++ emotesurvey/emotesurvey/urls.py
@@ -13,14 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.shortcuts import redirect
 from django.contrib import admin
 from django.urls import include, path
 from survey import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', lambda request: redirect('/survey')),
     path('survey', views.survey),
-    path('survey/thanks', views.thanks),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
--- emotesurvey/survey/views.py
+++ emotesurvey/survey/views.py
@@ -1,15 +1,5 @@
-from django.http import HttpResponse
-from django.template.response import TemplateResponse
 from django.shortcuts import render
-
-
-def index(request):
-    return TemplateResponse(request, "index.html")
 
 
 def survey(request):
     return render(request, "survey.html")
-
-
-def thanks(request):
-    return HttpResponse("<h2>Thanks!</h2>")
